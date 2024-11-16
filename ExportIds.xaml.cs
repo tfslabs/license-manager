09ef5e53749ed6e31869d7bc3492f1ae0b80ecf5
白いねこ <shiroineko.tfs@gmail.com>
2024-11-16 19:57:06 +0700

    resolve missing packages on build
    Co-Authored-By: Vo Luu Tuong Anh <anhvlt.2006@outlook.com>

--- ExportIds.xaml.cs
+++ ExportIds.xaml.cs
@@ -3,6 +3,7 @@ using HGM.Hotbird64.Vlmcs;
 using Microsoft.Win32;
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
