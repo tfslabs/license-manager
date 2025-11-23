df150206e1dff005ed6e73e6217064f34d08920a
Shiroi Neko <shiroineko.tfs@theflightsims.com>
2025-11-23 09:36:02 +0700

    update for guid
    Co-Authored-By: VLTA of @TheFlightSims <176574466+anhvlt-2k6@users.noreply.github.com>

--- App.xaml.cs
+++ App.xaml.cs
@@ -44,19 +44,20 @@ namespace HGM.Hotbird64.LicenseManager
 
         static App()
         {
-            // TODO: Get rid of this
-            KmsGuid win2008A = new("33e156e4-b76f-4a52-9f91-f641dd95ac48");
-            KmsGuid win2008B = new("8fe53387-3087-4447-8985-f75132215ac9");
-            KmsGuid win2008C = new("8a21fdf3-cbc5-44eb-83f3-fe284e6680a7");
-            KmsGuid win2008R2A = new("0fc6ccaf-ff0e-4fae-9d08-4370785bf7ed");
-            KmsGuid win2008R2B = new("ca87f5b6-cd46-40c0-b06d-8ecd57a4373f");
-            KmsGuid win2008R2C = new("b2ca2689-a9a8-42d7-938d-cf8e9f201958");
-            KmsGuid win2012 = new("8665cb71-468c-4aa3-a337-cb9bc9d5eaac");
-            KmsGuid win2012R2 = new("8456efd3-0c04-4089-8740-5b7238535a65");
-            KmsGuid win2016 = new("6e9fc069-257d-4bc4-b4a7-750514d32743");
-            KmsGuid win2019 = new("8449b1fb-f0ea-497a-99ab-66ca96e9a0f5");
-            KmsGuid win2022 = new("b74263e4-0f92-46c6-bcf8-c11d5efe2959");
-            ServerKmsGuids = [win2008A, win2008B, win2008C, win2008R2A, win2008R2B, win2008R2C, win2012, win2012R2, win2016, win2019, win2022];
+            ServerKmsGuids = [
+                new("33e156e4-b76f-4a52-9f91-f641dd95ac48"), //Windows Server 2008 A
+                new("8fe53387-3087-4447-8985-f75132215ac9"), //Windows Server 2008 B
+                new("8a21fdf3-cbc5-44eb-83f3-fe284e6680a7"), //Windows Server 2008 C
+                new("0fc6ccaf-ff0e-4fae-9d08-4370785bf7ed"), //Windows Server 2008 R2 A
+                new("ca87f5b6-cd46-40c0-b06d-8ecd57a4373f"), //Windows Server 2008 R2 B
+                new("b2ca2689-a9a8-42d7-938d-cf8e9f201958"), //Windows Server 2008 R2 C
+                new("8665cb71-468c-4aa3-a337-cb9bc9d5eaac"), //Windows Server 2012
+                new("8456efd3-0c04-4089-8740-5b7238535a65"), //Windows Server 2012 R2
+                new("6e9fc069-257d-4bc4-b4a7-750514d32743"), //Windows Server 2016
+                new("8449b1fb-f0ea-497a-99ab-66ca96e9a0f5"), //Windows Server 2019
+                new("b74263e4-0f92-46c6-bcf8-c11d5efe2959"), //Windows Server 2022
+                new("907f1f65-adcd-4a2e-95bc-4bf500bc6e58")  //Windows Server 2025
+            ];
         }
 
         private static bool TryLoadLibrary(string dllFileName)
